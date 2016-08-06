b33d8219104d25de3763d35c0b0d8807e3b895d4
nico1510 <Nicolas-Beck@gmx.de>
2016-08-06 14:14:52 +0200

    added google maps links

--- app.js
+++ app.js
@@ -10,6 +10,23 @@ const picsDir = path.join(staticPath, picsFolder);
 
 app.use(express.static(staticPath));
 
+
+function convertDMSToDD(degrees, minutes, seconds, direction) {
+    var dd = degrees + minutes/60 + seconds/(60*60);
+    if (direction == "S" || direction == "W") {
+        dd = dd * -1;
+    } // Don't do anything for N or E
+    return dd;
+}
+
+function extractCoordinates(gps) {
+    const lat = convertDMSToDD(...gps.GPSLatitude, gps.GPSLatitudeRef);
+    const long = convertDMSToDD(...gps.GPSLongitude, gps.GPSLongitudeRef);
+    const gmaps = `http://maps.google.com/maps?q=${lat},${long}`;
+    const streetview = `http://maps.google.com/maps?q=&layer=c&cbll=${lat},${long}`;
+    return {lat, long, gmaps, streetview};
+}
+
 app.get('/pos/:pic', function (req, res) {
     new ExifImage({image: path.join(__dirname, picsDir, req.params.pic)}, function (error, exifData) {
         if (error)
@@ -32,7 +49,8 @@ app.get('/list', function (req, res) {
                         else {
                             resolve({
                                 img: req.protocol + '://' + req.get('host') + path.join(picsFolder, path.basename(file)),
-                                gps: exifData.gps
+                                gps: extractCoordinates(exifData.gps),
+                                timestamp: exifData.GPSDateStamp
                             });
                         }
                     });
