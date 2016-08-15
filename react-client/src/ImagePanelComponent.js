5dff6804ae73164bd0deeddbc9e3879a798ffdfd
nico1510 <Nicolas-Beck@gmx.de>
2016-08-15 18:03:17 +0200

    styling

--- react-client/src/ImagePanelComponent.js
+++ react-client/src/ImagePanelComponent.js
@@ -1,12 +1,14 @@
 import React, {Component} from 'react';
 import './App.css';
+import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
+
 
 class ImagePanelComponent extends Component {
 
     render() {
         let key = 0;
         return (
-            <div className="image-panel">
+            <div className='image-panel' style={{backgroundColor: darkBaseTheme.palette.canvasColor}}>
                 {!this.props.list.length ? 'Loading...' : this.props.list.map((item) => {
                     return (
                         <div className='image-item' style={{ backgroundImage: `url('${item.src}')`}} key={key++}>
