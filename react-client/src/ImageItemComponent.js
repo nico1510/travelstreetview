3f8d575ea1f5ca0ef5ac4247f0fa5112fb191aab
nico1510 <Nicolas-Beck@gmx.de>
2016-09-16 21:54:29 +0200

    switched to inline styles with aphrodite

--- react-client/src/ImageItemComponent.js
+++ react-client/src/ImageItemComponent.js
@@ -1,6 +1,13 @@
 import React, {Component} from 'react';
 import {Dialog, FloatingActionButton, Badge} from 'material-ui';
+import {hasGPSattached} from './Utils';
 import MapsPlace from 'material-ui/svg-icons/maps/place';
+import NavigationClose from 'material-ui/svg-icons/navigation/close';
+import NavigationCancel from 'material-ui/svg-icons/navigation/cancel';
+import FlatButton from 'material-ui/FlatButton';
+import { StyleSheet, css } from 'aphrodite';
+import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
+import {red700 as errorColor} from 'material-ui/styles/colors'
 
 class ImageItemComponent extends Component {
 
@@ -24,36 +31,38 @@ class ImageItemComponent extends Component {
             <Badge
                 onClick={this.props.handleItemSelect.bind(this, this.props.item)}
                 badgeStyle={{top: '25px', right: '25px'}}
-                badgeContent={<FloatingActionButton>
-                                  <MapsPlace />
-                              </FloatingActionButton>}>
-                <div className={'image-item '
-                    + ((this.props.isSelected)? 'cyan-shadow' : 'black-shadow')
-                    + ((this.props.item.orientation === 6)? ' rotate-bg-img' : ' ')
-                        }
+                badgeContent={
+                    hasGPSattached(this.props.item)?
+                    <FloatingActionButton>
+                        <MapsPlace />
+                    </FloatingActionButton>
+                        :
+                    <FloatingActionButton backgroundColor={errorColor} tooltip="No GPS data available" tooltipPosition="bottom-right">
+                        <NavigationCancel />
+                    </FloatingActionButton>
+                    }>
+                <div className={css(styles.imageItem,
+                                this.props.isSelected? styles.highlightShadow : styles.blackShadow,
+                                (this.props.item.orientation === 6) && styles.rotateBgImg)}
                      style={{ backgroundImage: `url('${this.props.item.src}')`}}
                      onClick={this.handleImageClick}>
 
                     <Dialog
                         actions={[]}
-                        bodyStyle={{margin: '0px', padding: '0px'}}
+                        bodyClassName={css(styles.dialogBody)}
                         modal={false}
                         autoDetectWindowHeight={true}
-                        contentStyle={{
-                        width: '100%',
-                        maxWidth: 'none'}}
+                        contentClassName={css(styles.dialogContainer)}
                         open={this.state.dialogOpen}
                         onRequestClose={this.handleClose}>
-                        <div style={{
-                            height: '80vh',
-                            display: 'flex',
-                            width: '100%',
-                            justifyContent: 'center',
-                            alignItems: 'center'
-                        }}>
+                        <div className={css(styles.dialogContent)}>
+                            <FlatButton onClick={this.handleClose} className={css(styles.dialogCloseButton)}>
+                                <NavigationClose className={css(styles.closeIcon)} />
+                            </FlatButton>
+
                             <img onClick={this.handleClose} src={this.props.item.src}
                                  alt={this.props.item.src}
-                                 className={((this.props.item.orientation === 6)? ' rotated-full-img' : 'normal-full-img')} />
+                                 className={css((this.props.item.orientation === 6)? styles.rotatedFullImg : styles.normalFullImg)} />
                         </div>
                     </Dialog>
                 </div>
@@ -62,5 +71,69 @@ class ImageItemComponent extends Component {
     }
 }
 
+const styles = StyleSheet.create({
+    dialogContainer: {
+        width: '100%',
+        maxWidth: 'none'
+    },
+
+    dialogBody: {
+        margin: '0px',
+        padding: '0px'
+    },
+
+    dialogContent: {
+        height: '80vh',
+        display: 'flex',
+        width: '100%',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+
+    dialogCloseButton: {
+        position: 'absolute',
+        top: '10px',
+        left: '0px',
+        borderRadius: '50%'
+    },
+
+    closeIcon: {
+        padding: '5px'
+    },
+
+    imageItem: {
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+        borderRadius: '50px',
+        border: '1px solid darkslategray',
+        margin: '5px',
+        width: '230px',
+        height: '230px'
+    },
+
+    blackShadow: {
+        boxShadow: '0 6px 20px 0 rgba(0, 0, 0, 0.5)'
+    },
+
+    highlightShadow: {
+        boxShadow: `0 6px 30px 0 ${darkBaseTheme.palette.primary1Color}`
+    },
+
+    rotateBgImg: {
+        transform: 'rotate(90deg)'
+    },
+
+    rotatedFullImg: {
+        height: 'auto',
+        transform: 'rotate(90deg)',
+        width: '70vh'
+    },
+
+    normalFullImg: {
+        height: '70vh',
+        width: 'auto'
+    }
+
+});
 
 export default ImageItemComponent;
--- react-client/src/ImagePanelComponent.js
+++ react-client/src/ImagePanelComponent.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
 import ImageItemComponent from './ImageItemComponent';
+import { StyleSheet, css } from 'aphrodite/no-important';
 
 function ImagePanelComponent(props) {
     let key = 0;
     return (
-        <div className='image-panel' style={{backgroundColor: darkBaseTheme.palette.canvasColor}}>
+        <div className={css(styles.imagePanel)}>
             {!props.list.length ? 'Loading...' : props.list.map((item) => {
                 return (
                     <ImageItemComponent handleItemSelect={props.handleItemSelect} isSelected={item === props.selectedItem} item={item} key={key++}/>
@@ -14,6 +15,19 @@ function ImagePanelComponent(props) {
         </div>
     );
 }
+
+const styles = StyleSheet.create({
+    imagePanel: {
+        display: 'flex',
+        flexFlow: 'row nowrap',
+        width: '100 vw',
+        height: '100%',
+        overflow: 'scroll',
+        justifyContent: 'start',
+        alignItems: 'start',
+        backgroundColor: darkBaseTheme.palette.canvasColor
+    }
+});
 
 
 export default ImagePanelComponent;
