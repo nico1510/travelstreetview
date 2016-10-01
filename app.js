9a14836f9a28579a395f71601b9c14c4ec28a531
nico1510 <Nicolas-Beck@gmx.de>
2016-10-01 12:10:29 +0200

    removed cross domain handling

--- app.js
+++ app.js
@@ -36,15 +36,6 @@ const upload = multer({
     }
 });
 
-const allowCrossDomain = function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-
-    next();
-};
-
-app.use(allowCrossDomain);
 app.use(picsUrlFragment, express.static(staticPath));
 app.use(express.static(clientPath));
 
